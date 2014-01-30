2309967fdd2c0e1a07ddbf7eff1696ea7f2f1702
Rob Griffiths <rob@bytespider.eu>
2014-01-30 10:51:43 +0000

    make code more readable

--- src/psnr.js
+++ src/psnr.js
@@ -2,5 +2,9 @@ var mse = require('mse');
 var max = 255 * 255;
 
 module.exports = function psnr(original, noisy, step) {
-    return 10 * (Math.log(max / mse(original, noisy, step)) / Math.LN10);
+    return 10 * log10(max / mse(original, noisy, step);
 };
+
+function log10(value) {
+    return Math.log(value) / Math.LN10;
+}
