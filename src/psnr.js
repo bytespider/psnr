66f8fed16cea719db312ea913fa06f07df4c7744
Rob Griffiths <rob@bytespider.eu>
2014-07-22 08:48:54 +0100

    fixed syntax error

--- src/psnr.js
+++ src/psnr.js
@@ -2,7 +2,7 @@ var mse = require('mse');
 var max = 255 * 255;
 
 module.exports = function psnr(original, noisy, step) {
-    return 10 * log10(max / mse(original, noisy, step);
+    return 10 * log10(max / mse(original, noisy, step));
 };
 
 function log10(value) {
